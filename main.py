957c21ac97ba2293d239a0753f56f8bb0cab3b0e
Ufuk Güzel <ufukgzl@web.de>
2025-04-04 00:21:02 +0200

    GameOver!

--- main.py
+++ main.py
@@ -1,46 +1,89 @@
 import turtle
 import random
 
-t = turtle.Turtle()
+screen = turtle.Screen()
+turtle.bgcolor("light green")
+FONT = ("Arial", 24, "normal")
+score = 0
+game_over = False
+scor_turtle = None
 
-t.color("light green")
+countdown_turtle = turtle.Turtle()
+countdown_turtle.color("red")
 
-turtle.shape("turtle")
-turtle.color("red")
-turtle.pensize(10)
+turtle_turtle = turtle.Turtle()
 
-FONT = ('Arial', 30, 'normal')
+
+
+def score_turtle():
+    top_height = screen.window_height() / 2
+    y = top_height * 0.85
+
+    global scor_turtle
+    scor_turtle = turtle.Turtle()
+    scor_turtle.color("dark orange")
+    scor_turtle.hideturtle()
+    scor_turtle.penup()
+    scor_turtle.goto(0, y)
+    scor_turtle.write("Score: 0", align="center", font=FONT)
+
+
+def turtle_maker():
+    global turtle_turtle
+
+    def hand_click(x, y):
+        #print (x,y)
+        global score
+        score += 1
+        scor_turtle.clear()
+        scor_turtle.write("Score: {}".format(score), align="center", font=FONT)
+
+    turtle_turtle.onclick(hand_click)
+    turtle_turtle.color("dark green")
+    turtle_turtle.shape("turtle")
+    turtle_turtle.turtlesize(2)
+    turtle_turtle.penup()
+
+    def turtle_move():
+        if not game_over:
+            x = random.randint(-280, 280)
+            y = random.randint(-280, 280)
+            turtle_turtle.hideturtle()
+            turtle_turtle.goto(x, y)
+            turtle_turtle.showturtle()
+            turtle.ontimer(turtle_move, 800)
+
+    turtle_move()
+
+
 
 def countdown(time):
-    screen.onclick(None)  # disable click until countdown completes
-    turtle.clear()
-
-    if time > 0:
-        t.color("red")
-        t.penup()
-        t.goto(0, 250)
-        t.write(time, align='center', font=FONT)
-        screen.ontimer(lambda: countdown(time - 1), 1000)
+    global turtle_turtle
+    global game_over
+    countdown_turtle.hideturtle()
+    countdown_turtle.penup()
+    countdown_turtle.goto(0,220)
+    countdown_turtle.clear()
 
 
-        def move_random():
-            x = random.randint(-300, 300)
-            y = random.randint(-300, 300)
-            turtle.penup()
-            turtle.goto(x, y)
-
-            t.clear()
-
-        for i in range(10):
-            move_random()
+    if time > 0 :
+        countdown_turtle.clear()
+        countdown_turtle.write("Time:  {}".format(time), align="center", font=FONT)
+        screen.ontimer(lambda: countdown(time-1), 1000)
 
 
-    else:
-        turtle.write("Click Screen", align='center', font=FONT)
-        screen.onclick(lambda x, y: countdown(30))
+    else :
+        game_over = True
+        countdown_turtle.clear()
+        turtle_turtle.hideturtle()
+        countdown_turtle.write("Game Over !", align = "center", font = FONT )
 
-turtle.write("Click Screen", align= "center", font=FONT)
+turtle.tracer(0)
 
-screen = turtle.Screen()
-screen.onclick(lambda x, y: countdown(30))
-screen.mainloop()
+score_turtle()
+countdown(10)
+turtle_maker()
+
+turtle.tracer(1)
+
+turtle.mainloop()
